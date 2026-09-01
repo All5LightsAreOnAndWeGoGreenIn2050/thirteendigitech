5ed52e165a53d95b6e5c05da0978f5dea5d94575
All5LightsAreOnAndWeGoGreenIn2050 <bzhang@students.mags.school.nz>
2026-09-02 09:31:26 +1200

    bug fixes pong final

--- sports-game/scripts/player_pong.gd
+++ sports-game/scripts/player_pong.gd
@@ -34,10 +34,11 @@ func _physics_process(_delta: float) -> void:
 
 	velocity.y = y_input * player_speed
 	velocity.x = x_input * player_speed
-	move_and_slide()
 	# Restrict player to certain boundaries
 	position.x = clamp(position.x, boundary_left_x, boundary_right_x)
 	position.y = clamp(position.y, boundary_top_y, boundary_bottom_y)
+	move_and_slide()
+
 
 # Detect player hitting input
 func _input(event: InputEvent) -> void:
