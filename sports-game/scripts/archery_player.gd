d5485ff8422c28aaa276197dc6ce4461896613dc
All5LightsAreOnAndWeGoGreenIn2050 <bzhang@students.mags.school.nz>
2026-09-18 19:31:30 +1200

    archery update

--- sports-game/scripts/archery_player.gd
+++ sports-game/scripts/archery_player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+const gravity = 1000.0
 
 @export var move_speed: float = 200.0
 @export var arrow_cooldown: float = 1.0
@@ -17,6 +18,59 @@ func _ready() -> void:
 	cooldown_timer.wait_time = arrow_cooldown
 	cooldown_timer.one_shot = true
 	cooldown_timer.timeout.connect(cooldown_finished)
+	
+func _process(delta: float) -> void:
+	move(delta)
+	aim()
+	aim_target_update()
+	
+
+func move(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	var direction = Input.get_axis("player1_left","player2_right")
+	
+	velocity.x = direction * move_speed
+	
+	move_and_slide()
+# Copy of ping pong clamps to do position
+
+func aim() -> void:
+	if not is_instance_valid(bow_turn):
+		return
+	var mouse_position = get_global_mouse_position()
+	bow_turn.look_at(mouse_position)
+	
+
+func aim_target_update() -> void:
+	aim_guide.global_position = get_global_mouse_position()
+	
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and can_fire:
+			fire()
+			
+			
+func fire() -> void:
+	can_fire = false
+	cooldown_timer.start()
+	
+	var arrow: Node2D = arrow.instantiate()
+	arrow.shooter_id = "player"
+	get_parent().add_child(arrow)
+	
+	var arrow_position = Vector2(16,0)
+	if is_instance_valid(bow_turn):
+		arrow.global_position = bow_turn.global_position + \
+		bow_turn.transform.x * arrow_position.x
+		arrow.rotation = bow_turn.global_rotation
+	else:
+		arrow.global_position = Vector2(40, -10)
+		
+	var arrow_direction: = (get_global_mouse_position() - arrow.global_position).normalized()
+	arrow.launch(arrow_direction, arrow_speed)
 	
 	
 func cooldown_finished() -> void:
--- sports-game/scripts/arrow.gd
+++ sports-game/scripts/arrow.gd
@@ -1,11 +1,52 @@
 extends Node2D
 
+signal landed (arrow: Node2D)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var speed: float = 900.0
+@export var gravity: = 900.0
+@export var shooter_id: = "player"
 
+@onready var arrow_sprite: Sprite2D = $Sprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var velocity: Vector2 = Vector2.ZERO
+var fly: bool = false
+var target_node: Node2D = null
+
+func launch(direction: Vector2, launch_speed: float = speed) -> void:
+	velocity = direction.normalized() * launch_speed
+	fly = true
+		
 func _process(delta: float) -> void:
-	pass
+	if not fly:
+		return
+		
+	velocity.y += gravity * delta
+	global_position += velocity * delta
+	
+	rotation = velocity.angle()
+	
+	if target_node and target_node.is_inside_tree():
+		var arrow_distance = global_position.distance_to(target_node.global_position)
+		if arrow_distance < 90:
+			fly = false
+			target_node.try_hit(global_position, shooter_id)
+			emit_signal("landed", self)
+			stick_in_target()
+			return
+			
+	var cleanup = get_viewport_rect().size
+	if global_position.y > cleanup.y + 100 or \
+		global_position.x < -100 or \
+		global_position.x > cleanup.x + 100:
+		fly = false
+		emit_signal("landed", self)
+		queue_free()
+		
+func stick_in_target() -> void:
+	set_process(false)
+	var arrow_tween = create_tween()
+	arrow_tween.tween_interval(1.2)
+	arrow_tween.tween_property(self, "modulate:a", 0.0, 0.4)
+	arrow_tween.tween_callback(queue_free)
+			
+	
